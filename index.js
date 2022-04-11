c47956776e0c4edef8c79504d52b8b84b6c449a1
Alex McDaniel <alexpmcdaniel@gmail.com>
2022-04-11 12:16:55 -0400

    commit tree e22547437470e46a8363822d408a3cbdfd32617f

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let pThing = document.getElementById('text')
+    pThing.textContent = 'This is really cool!'
+    console.log(pThing.textContent)
+  });
